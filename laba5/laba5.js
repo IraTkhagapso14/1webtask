4446582024867947acebb460280b24262d870ccb
IraTkhagapso14 <144713252+IraTkhagapso14@users.noreply.github.com>
2023-10-19 17:36:56 +0300

    Update laba5.js

--- laba5/laba5.js
+++ laba5/laba5.js
@@ -1,15 +1,18 @@
 const productSelect = document.getElementById("product");
 const quantityInput = document.getElementById("quantity");
 const totalSpan = document.getElementById("total");
-const prices = { item1: 5,item2: 7,item3: 10};
+const prices = { item1: 200, item2: 300, item3: 400 };
 function calculateTotal() {
     const selectedProduct = productSelect.value;
-    const quantity = parseInt(quantityInput.value);
-    if (selectedProduct in prices && !isNaN(quantity) && quantity > 0) {
+    const quantityValue = quantityInput.value; // Get the quantity as a string
+    if (selectedProduct in prices && !isNaN(quantityValue) && quantityValue > 0) {
         const price = prices[selectedProduct];
+        const quantity = parseInt(quantityValue); // Parse the quantity as an integer
         const total = price * quantity;
-        totalSpan.textContent = `$${total}`;
+        totalSpan.textContent = total + " ₽";
     } else {
         totalSpan.textContent = "Ошибка ввода";
     }
 }
+const calculateButton = document.getElementById("calculate");
+calculateButton.addEventListener("click", calculateTotal);
