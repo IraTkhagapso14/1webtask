8a6d594ea4d54e882442f533b6dbb56f45a1e4df
IraTkhagapso14 <144713252+IraTkhagapso14@users.noreply.github.com>
2023-10-19 18:03:05 +0300

    Update laba5.js

--- laba5/laba5.js
+++ laba5/laba5.js
@@ -1,18 +1,22 @@
-const productSelect = document.getElementById("product");
-const quantityInput = document.getElementById("quantity");
-const totalSpan = document.getElementById("total");
-const prices = { item1: 200, item2: 300, item3: 400 };
-function calculateTotal() {
-    const selectedProduct = productSelect.value;
-    const quantityValue = quantityInput.value; 
-    if (selectedProduct in prices && !isNaN(quantityValue) && quantityValue > 0) {
-        const price = prices[selectedProduct];
-        const quantity = parseInt(quantityValue); 
-        const total = price * quantity;
-        totalSpan.textContent = total + " ₽";
-    } else {
-        totalSpan.textContent = "Ошибка ввода";
+    function calculateTotal() {
+        const productSelect = document.getElementById("product");
+        const quantityInput = document.getElementById("quantity");
+        const totalSpan = document.getElementById("total");
+        const prices = { item1: 200, item2: 300, item3: 400 };
+        const selectedProduct = productSelect.value;
+        const quantityValue = quantityInput.value;
+
+        if (selectedProduct in prices && !isNaN(quantityValue) && quantityValue > 0) {
+            const price = prices[selectedProduct];
+            const quantity = parseInt(quantityValue);
+            const total = price * quantity;
+            totalSpan.textContent = total + " ₽";
+        } else {
+            totalSpan.textContent = "Ошибка ввода";
+        }
     }
-}
-const calculateButton = document.getElementById("calculate");
-calculateButton.addEventListener("click", calculateTotal);
+
+    window.addEventListener("DOMContentLoaded", function (event) {
+        const calculateButton = document.getElementById("calculate");
+        calculateButton.addEventListener("click", calculateTotal);
+    });
