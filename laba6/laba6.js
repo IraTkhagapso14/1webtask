5ce87ea59f9aed9a4052392f46bcb6d3098084ed
IraTkhagapso14 <144713252+IraTkhagapso14@users.noreply.github.com>
2023-10-29 20:17:30 +0300

    Update laba6.js

--- laba6/laba6.js
+++ laba6/laba6.js
@@ -1,49 +1,62 @@
-const kolvoInput = document.getElementById("kolvo");
+ const kolvoInput = document.getElementById("kolvo");
+    const kolvobutton = document.getElementById("resetKolvo");
     const servradio = document.querySelectorAll('input[name="service"]');
-    const tovaroption = document.querySelector(".dopoptions");
-    const dopusluga = document.querySelector(".dopoprop");
-    const finalsum = document.getElementById("totalCost");
+    const prodoptions = document.querySelector(".prodoption");
+    const dopprops = document.querySelector(".dopproperty");
+    const finalcost = document.getElementById("totalCost");
+    const errormeselem = document.getElementById("errorMessage");
 
     const prices = { service1: 10, service2: 15, service3: 20 };
-    const tsenanaprod = { product1: 200, product2: 300, product3: 400 };
+    const pricesprod = { product1: 200, product2: 300, product3: 400 };
 
     function itog() {
-        const chosenservice = document.querySelector('input[name="service"]:checked').value;
-        const kolvo = Number(kolvoInput.value);
+        const chosenserv = document.querySelector('input[name="service"]:checked').value;
+        const kolvo = kolvoInput.value;
 
-        if (chosenservice) {
-            let total = 0;
-
-            if (chosenservice === "service2") {
-                const selectedProduct = tovaroption.querySelector("select").value;
-                const productPrice = tsenanaprod[selectedProduct];
-                total = prices[chosenservice] * kolvo + productPrice;
-            } else if (chosenservice === "service3") {
-                total = dopusluga.querySelector("input").checked
-                    ? 2 * prices[chosenservice] * kolvo
-                    : prices[chosenservice] * kolvo;
-            } else {
-                total = prices[chosenservice] * kolvo;
-            }
-
-            finalsum.textContent = total;
+        if (kolvo < 0) {
+            errormeselem.textContent = "Ошибка ввода: введите положительное число.";
+            return;
+        } else {
+            errormeselem.textContent = "";
         }
 
-        if (chosenservice === "service2") {
-            tovaroption.style.display = "block";
-            dopusluga.style.display = "none";
-        } else if (chosenservice === "service3") {
-            tovaroption.style.display = "none";
-            dopusluga.style.display = "block";
+        if (chosenserv) {
+            let total = 0;
+
+            if (chosenserv === "service2") {
+                const selectedProduct = prodoptions.querySelector("select").value;
+                const productPrice = pricesprod[selectedProduct];
+                total = prices[chosenserv] * kolvo + productPrice;
+            } else if (chosenserv === "service3") {
+                total = dopprops.querySelector("input").checked
+                    ? 2 * prices[chosenserv] * kolvo
+                    : prices[chosenserv] * kolvo;
+            } else {
+                total = prices[chosenserv] * kolvo;
+            }
+
+            finalcost.textContent = total;
+        }
+
+        if (chosenserv === "service2") {
+            prodoptions.style.display = "block";
+            dopprops.style.display = "none";
+        } else if (chosenserv === "service3") {
+            prodoptions.style.display = "none";
+            dopprops.style.display = "block";
         } else {
-            tovaroption.style.display = "none";
-            dopusluga.style.display = "none";
+            prodoptions.style.display = "none";
+            dopprops.style.display = "none";
         }
     }
 
     kolvoInput.addEventListener("input", itog);
+    kolvobutton.addEventListener("click", () => {
+        kolvoInput.value = "";
+        errormeselem.textContent = "";
+    });
     servradio.forEach((radio) => radio.addEventListener("change", itog));
-    dopusluga.querySelector("input").addEventListener("change", itog);
-    tovaroption.querySelector("select").addEventListener("change", itog);
+    dopprops.querySelector("input").addEventListener("change", itog);
+    prodoptions.querySelector("select").addEventListener("change", itog);
 
     itog();
